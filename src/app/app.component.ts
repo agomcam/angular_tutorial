a8c6f1a2e0b96105dea8badf576fa73868554a71
agomcam <agomcam813@iescarrillo.es>
2024-11-11 16:45:56 +0100

    Primeros pasos gestor de tareas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { SiginComponent } from './components/auth/sigin/sigin.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBarComponent, FooterComponent, LoginComponent, SiginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular_tutorial';
+  nombre = 'Antonio Gómez';
 }
