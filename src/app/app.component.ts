75e388dd8bcba474ac16e643fde9cca88ab25b31
agomcam <agomcam813@iescarrillo.es>
2024-11-17 16:25:05 +0100

    Vista desde movil corregido (Tarjetas)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LoginComponent } from './components/auth/login/login.component';
 import { SiginComponent } from './components/auth/sigin/sigin.component';
+import { TasklistComponent } from "./components/task/tasklist/tasklist.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent, FooterComponent, LoginComponent, SiginComponent],
+  imports: [RouterOutlet, NavBarComponent, FooterComponent, LoginComponent, SiginComponent, RouterLink, TasklistComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import path from 'path';
+import { LoginComponent } from './components/auth/login/login.component';
+import { SiginComponent } from './components/auth/sigin/sigin.component';
+import { TasklistComponent } from './components/task/tasklist/tasklist.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   {path:'login',component:LoginComponent},
+   {path:'sigin',component:SiginComponent},
+   {path:'tasklist',component:TasklistComponent}
+   // {path:'',redirectTo:'/tasklist',pathMatch:'full'}
+];
