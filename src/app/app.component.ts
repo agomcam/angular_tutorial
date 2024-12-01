f2173e910b96675aa5f774cd68ea72d6381dba58
agomcam <agomcam813@iescarrillo.es>
2024-12-01 13:30:35 +0100

    Ejercicio 8 01/12/2024

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
-import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { LoginComponent } from './pages/auth/login/login.component';
-import { SiginComponent } from './pages/auth/sigin/sigin.component';
-import {TasklistComponent} from "./components/task/tasklist/tasklist.component";
-import {FormsModule, ReactiveFormsModule} from '@angular/forms'
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {FormsModule} from '@angular/forms'
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FormsModule, NavBarComponent, FooterComponent, LoginComponent, SiginComponent, RouterLink, TasklistComponent],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-tutorial-app';
-  imagen: string = "";
-  url: string = "";
-
-  imgAleatoria() {
-    this.imagen = "https://picsum.photos/200/300?random=" + Math.round(Math.random() * 1000)
-  }
-
 }
--- src/app/components/task/tasklist/tasklist.component.ts
+++ src/app/components/task/tasklist/tasklist.component.ts
@@ -1,9 +1,10 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Task, TaskPriority, TaskStatus} from '../../../models/task.models';
 import {CommonModule} from '@angular/common';
 import {ResumeComponent} from '../resume/resume.component';
 import {TaskEvent} from '../../../models/TaskEvent.models';
 import {TaskformComponent} from '../taskform/taskform.component';
+import {TaskService} from '../../../services/task.service';
 
 @Component({
   selector: 'app-tasklist',
@@ -12,113 +13,32 @@ import {TaskformComponent} from '../taskform/taskform.component';
   templateUrl: './tasklist.component.html',
   styleUrl: './tasklist.component.css'
 })
-export class TasklistComponent {
-  tasklist: Task[] = [
-    new Task(1, "Tarea 1", "Descripción Tarea 1", TaskPriority.LOW, TaskStatus.PENDING, new Date("11/1/2024"), new Date("11/18/2024"), false),
-    new Task(2, "Tarea 2", "Descripción Tarea 2", TaskPriority.HIGH, TaskStatus.IN_PROGRESS, new Date("11/5/2024"), new Date("11/16/2024"), false),
-    new Task(3, "Tarea 3", "Descripción Tarea 3", TaskPriority.LOW, TaskStatus.IN_PROGRESS, new Date("11/21/2024"), new Date("11/30/2024"), false),
-    new Task(4, "Tarea 4", "Descripción Tarea 4", TaskPriority.HIGH, TaskStatus.COMPLETED, new Date("11/8/2024"), new Date("11/21/2024"), false),
-    new Task(5, "Tarea 5", "Descripción Tarea 5", TaskPriority.MEDIUM, TaskStatus.PENDING, new Date("11/10/2024"), new Date("11/30/2024"), false)
-  ]
+export class TasklistComponent implements OnInit {
 
+  tasklist: Task[] = [];
+
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.tasklist = this.taskService.getTasks()
+  }
 
   taskToEdit: Task | null = null; // Tarea actualmente en edición
 
   addNewTask(task: Task) {
-    this.tasklist.push(task); // Añadir la nueva tarea al array
+    this.taskService.addNewTask(task)
   }
 
   saveTask(updatedTask: Task) {
-    const index = this.tasklist.findIndex(task => task.id === updatedTask.id);
-    if (index > -1) {
-      this.tasklist[index] = updatedTask; // Reemplazar la tarea existente
-    }
-    this.taskToEdit = null; // Limpiar el modo de edición
+    this.taskService.saveTask(updatedTask)
   }
 
   setTaskToEdit(task: Task) {
-    this.taskToEdit = task; // Asignar la tarea seleccionada al formulario
+    this.taskService.setTaskToEdit(task)
   }
 
 
   modifyTask(taskEvent: TaskEvent) {
-
-    switch (taskEvent.action) {
-      case "raiseifpriority":
-        this.raiseifpriority(taskEvent.taskId);
-        break;
-
-      case "deleteTask":
-        this.deleteTask(taskEvent.taskId);
-        break;
-
-      case "lowerPriority":
-        this.lowerPriority(taskEvent.taskId);
-        break;
-
-      case "setStatus":
-        this.setStatus(taskEvent.taskId);
-        break;
-
-      default:
-        break;
-    }
-  }
-
-  setStatus(id: number) {
-    for (let index = 0; index < this.tasklist.length; index++) {
-      if (this.tasklist[index].id == id) {
-        if (this.tasklist[index].status == TaskStatus.COMPLETED) {
-          this.tasklist[index].status = TaskStatus.PENDING
-        } else if (this.tasklist[index].status == TaskStatus.IN_PROGRESS) {
-          this.tasklist[index].status = TaskStatus.COMPLETED
-        } else {
-          this.tasklist[index].status = TaskStatus.IN_PROGRESS
-        }
-
-        break;
-      }
-    }
-
-  }
-
-  deleteTask(id: number) {
-    for (let index = 0; index < this.tasklist.length; index++) {
-      if (this.tasklist[index].id == id) {
-        this.tasklist[index].isDelete = true;
-        break;
-      }
-
-    }
-  }
-
-
-  lowerPriority(id: number) {
-    for (let index = 0; index < this.tasklist.length; index++) {
-      if (this.tasklist[index].id == id) {
-        if (this.tasklist[index].priority == TaskPriority.HIGH) {
-          this.tasklist[index].priority = TaskPriority.MEDIUM
-        } else if (this.tasklist[index].priority == TaskPriority.MEDIUM) {
-          this.tasklist[index].priority = TaskPriority.LOW
-        }
-
-        break;
-      }
-    }
-  }
-
-
-  raiseifpriority(id: number) {
-    for (let index = 0; index < this.tasklist.length; index++) {
-      if (this.tasklist[index].id == id) {
-        if (this.tasklist[index].priority == TaskPriority.MEDIUM) {
-          this.tasklist[index].priority = TaskPriority.HIGH
-        } else if (this.tasklist[index].priority == TaskPriority.LOW) {
-          this.tasklist[index].priority = TaskPriority.MEDIUM
-        }
-
-        break;
-      }
-    }
+    this.taskService.modifyTask(taskEvent)
   }
 }
