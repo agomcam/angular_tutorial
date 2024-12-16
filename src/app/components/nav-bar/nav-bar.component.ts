a9c05bbcf9addd946b0c48fe65015351acc5d6dc
Antonio <agomcam813@iescarrillo.es>
2024-12-16 16:06:25 +0100

    prueba

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,13 +1,12 @@
 import {Component} from '@angular/core';
 import {Router, RouterLink} from '@angular/router';
 import {AuthService} from '../../services/auth.service';
-import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
   imports: [
-    RouterLink, CommonModule
+    RouterLink
   ],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
