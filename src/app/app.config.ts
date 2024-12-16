8d6292bd83fb53b511bbb7a5ccad61b79723f4d4
Antonio <agomcam813@iescarrillo.es>
2024-12-16 13:38:49 +0100

    Cambios

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,9 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideFirebaseApp(() => initializeApp({ "projectId": "angular-tutorial-f9f07", "appId": "1:239544362482:web:c132097512464cb5f0776b", "storageBucket": "angular-tutorial-f9f07.firebasestorage.app", "apiKey": "AIzaSyDBYaql5wnd0YAQyj1xATzcVu_PH7-F11s", "authDomain": "angular-tutorial-f9f07.firebaseapp.com", "messagingSenderId": "239544362482" })), provideAuth(() => getAuth())]
 };
