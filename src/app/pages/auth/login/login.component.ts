92d87572993ad0eb045239edbf6e87f040a0c6ad
Antonio <agomcam813@iescarrillo.es>
2024-12-16 13:12:30 +0100

    Cambios

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,12 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  formLogin: FormGroup;
+  constructor(formBuilder: FormBuilder) {
+    this.formLogin = formBuilder.group({
+      'email': ['', [Validators.required,Validators.email]],
+      'password':['',[Validators.required]]
+    })
+  }
 
+  OnSubmit(){
+    if (this.formLogin.valid) {
+      console.log("El login es valido")
+    }else{
+      console.log("El login es invalido")
+    }
+  }
 }
