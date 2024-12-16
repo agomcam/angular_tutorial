f4374aa70c66e3c4b2b0e904e9774ec28fadfd07
Antonio <agomcam813@iescarrillo.es>
2024-12-16 15:49:35 +0100

    prueba

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,20 +1,21 @@
-import { Component } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
-import { AuthService } from '../../services/auth.service';
-import { CommonModule } from '@angular/common';
+import {Component} from '@angular/core';
+import {Router, RouterLink} from '@angular/router';
+import {AuthService} from '../../services/auth.service';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink, CommonModule
   ],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
 
-  constructor(private router: Router, private authService: AuthService) { }
+  constructor(private router: Router, private authService: AuthService) {
+  }
 
   singout() {
     this.authService.logout();
